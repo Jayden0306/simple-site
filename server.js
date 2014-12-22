dbaaf2b42082ea8b1f09408a722c44afec60e87b
jayden0306 <feng6680946@hotmail.com>
2014-12-21 17:11:03 -0800

    inpiration Ajax

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express"),
 	app = express();
 
-app.get("/", function (req, res) {
-	res.send("hello universe!");
+app.get("/", function (req, res){
+  res.sendfile('./hello.html');
 });
 
 var quotes = [
